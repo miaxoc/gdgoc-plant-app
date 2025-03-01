42020bb7f6245f298c1b1b7f73ff3da85471c378
Mia OConnell <miaxoconnell@gmail.com>
2025-03-01 16:17:07 +0900

    wip

--- app/add-plant-1.tsx
+++ app/add-plant-1.tsx
@@ -1,2 +1,3 @@
 import React from "react";
 import { View, Text, FlatList, Image, TouchableOpacity, StyleSheet, ScrollView } from "react-native";
+import { RelativePathString, useRouter } from "expo-router";
