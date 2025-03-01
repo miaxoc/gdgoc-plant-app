e59694b75d0b66e49178419bc964a1f91ba97ba8
Mia OConnell <miaxoconnell@gmail.com>
2025-03-01 16:20:05 +0900

    wip

--- app/add-plant-1.tsx
+++ app/add-plant-1.tsx
@@ -1,3 +1,14 @@
 import React from "react";
 import { View, Text, FlatList, Image, TouchableOpacity, StyleSheet, ScrollView } from "react-native";
 import { RelativePathString, useRouter } from "expo-router";
+
+export default function HomeScreen() {
+
+  const router = useRouter();
+
+  const AddPlantScreen = () => {
+
+
+  }
+
+}
